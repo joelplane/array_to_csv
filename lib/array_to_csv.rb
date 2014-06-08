6ce9a8ef16ca967b6d53c1c6ab40e6f335b9a232
Joel Plane <joel.plane@gmail.com>
2014-06-08 16:59:05 +1000

    core functionality in ArrayToCsv class

--- lib/array_to_csv.rb
+++ lib/array_to_csv.rb
@@ -1,3 +1,49 @@
 class ArrayToCsv
+
+  def initialize array, csv_lib=choose_csv_lib
+    @array = array
+    @csv = csv_lib
+  end
+
+  def to_csv
+    @csv.generate do |csv|
+      each_row do |row|
+        csv << row
+      end
+    end
+  end
+
+  private
+
+  def each_row
+    yield head
+    @array.each do |hash|
+      yield row_from_hash hash
+    end
+  end
+
+  def head
+    @head_from_array ||= [].tap do |keys|
+      seen_keys = {}
+      @array.each do |hash|
+        hash.keys.each do |key|
+          unless seen_keys[key]
+            seen_keys[key] = true
+            keys << key
+          end
+        end
+      end
+    end
+  end
+
+  def row_from_hash hash
+    hash.values_at(*head)
+  end
+
+  # TODO: return FasterCSV in old ruby
+  def choose_csv_lib
+    CSV
+  end
+
 end
 
---
+++ spec/array_to_csv_spec.rb
@@ -0,0 +1,40 @@
+require 'csv'
+require 'array_to_csv'
+
+describe ArrayToCsv do
+
+  describe ".to_csv" do
+
+    context "given an empty array" do
+      it "should produce an empty CSV" do
+        expect(ArrayToCsv.new([]).to_csv).to eq("\n")
+      end
+    end
+
+    context "given an array of one empty hash" do
+      it "should produce an empty CSV" do
+        expect(ArrayToCsv.new([{}]).to_csv).to eq("\n\n")
+      end
+    end
+
+    context "given an array of one hash" do
+      it "should produce a one-value-row CSV" do
+        expect(ArrayToCsv.new([{:key => 'value'}]).to_csv).to eq("key\nvalue\n")
+      end
+    end
+
+    context "given an array of two hashes" do
+      it "should produce a CSV with two value rows" do
+        expect(ArrayToCsv.new([{:key => 'value1'}, {:key => 'value2'}]).to_csv).to eq("key\nvalue1\nvalue2\n")
+      end
+    end
+
+    context "given an array of two hashes with different keys" do
+      it "should produce a CSV with column headings combined from both hash keys, and two value rows" do
+        expect(ArrayToCsv.new([{:key1 => 'value1'}, {:key2 => 'value2'}]).to_csv).to eq("key1,key2\nvalue1,\n,value2\n")
+      end
+    end
+
+  end
+
+end
